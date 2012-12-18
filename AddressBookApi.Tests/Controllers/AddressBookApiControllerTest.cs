617f4e6113b64e7de9f9a1c29b787c66c64f1dd3
Dileepa <dnirosh@gmail.com>
2012-12-18 11:26:17 +0530

    template changed to WebApi
    template changed to WebApi ,and started with Knockout implementation

---
+++ AddressBookApi.Tests/Controllers/AddressBookApiControllerTest.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using AddressBookApi.Controllers;
+using AddressBookApi.Models;
+using System.Collections.Generic;
+
+namespace AddressBookApi.Tests
+{
+    [TestClass]
+    public class AddressBookApiControllerTest
+    {
+        [TestMethod]
+        public void Get()
+        {
+            AddressBookApiController testController = new AddressBookApiController();
+            IEnumerable<AddressBook> AddressBookResult = testController.GetAddressBook();
+
+            // Assert
+            Assert.IsNotNull(AddressBookResult);
+            int itemCount = 0;
+
+            foreach (var addressBookEntry in AddressBookResult)
+            {
+                itemCount++;
+                Assert.IsTrue(addressBookEntry.Id == 1);
+                Assert.IsTrue(addressBookEntry.Name == "Dileepa");
+                Assert.IsTrue(addressBookEntry.Address == "Earth");
+            }
+
+            Assert.IsTrue(itemCount == 1);
+
+        }
+    }
+}
---
+++ AddressBookApi/Controllers/AddressBookApiController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using AddressBookApi.Models;
+
+namespace AddressBookApi.Controllers
+{
+    public class AddressBookApiController : ApiController
+    {
+        List<AddressBook> addressBooks = new List<AddressBook>();
+        public AddressBookApiController()
+        {
+            addressBooks.Add(new AddressBook{ Id = 1, Name = "Dileepa" ,Address = "Earth" });
+
+        }
+
+        // GET api/addressbookapi
+        public IEnumerable<AddressBook> GetAddressBook()
+        {
+            return addressBooks;
+        }
+
+        // GET api/addressbookapi/5
+        public AddressBook GetAddressBook(int id)
+        {
+            var address = addressBooks.FirstOrDefault(a => a.Id == id);
+            return address;
+        }
+
+        // POST api/addressbookapi
+        public void PostAddressBook([FromBody]string value)
+        {
+
+        }
+
+        // PUT api/addressbookapi/5
+        public void PutAddressBook(int id, [FromBody]string value)
+        {
+
+        }
+
+        // DELETE api/addressbookapi/5
+        public void DeleteAddressBook(int id)
+        {
+        }
+    }
+}

e6d822d1f037d416226db416de3e9da8ee499d2f
Dileepa <dnirosh@gmail.com>
2013-01-03 10:59:06 +0530

    added a method to test input values
    added a method to test input values

--- AddressBookApi/UnitTests/SeleniumUnitTest.cs
+++ AddressBookApi/UnitTests/SeleniumUnitTest.cs
@@ -32,6 +32,7 @@ namespace AddressBookApi.Tests
             driver.Quit();
         }
 
+        /*
         [Test]
         public void GoogleSearch()
         {
@@ -49,15 +50,48 @@ namespace AddressBookApi.Tests
             wait.Until((d) => { return d.Title.StartsWith("selenium"); });
             //Check that the Title is what we are expecting
             Assert.AreEqual("selenium - Google Search", driver.Title);
-        }
+        }*/
         
         [Test]
         public void AddressBookTest()
         {
             //Navigate to the site
-            driver.Navigate().GoToUrl("http://localhost:41985/api/AddressBookApi/");
+            driver.Navigate().GoToUrl("http://localhost:41985/");
+            //driver.FindElements(By.CssSelector("table tbody tr td:nth-child(1)").Select(td => td.Text).Should().Contain("Darth Vader"));
+            //bool hasExpectedValueCheck = driver.FindElements(By.CssSelector("tbody#contacts tr td:nth-child(1)")).Select(td => td.Text).Single().Contains("aaa");
+            //Assert.AreEqual(true, hasExpectedValueCheck);
 
-        
+            //IWebElement table = driver.FindElement(By.ClassName("list"));
+            IWebElement table = driver.FindElement(By.Id("addressBookTable"));
+            //IWebElement tbody = table.FindElement(By.TagName("tbody"));
+
+            IList<IWebElement> rows = table.FindElements(By.TagName("tr"));
+            IList<IWebElement> tds = rows.ElementAt(1).FindElements(By.TagName("td"));
+
+            IWebElement inputName ,inputAddress;
+            if (tds.Count > 0)
+            {
+                /*WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(5));
+                wait.Until(p => p.FindElement(By.TagName("input")).GetAttribute("value") == "Dileepa");
+                */
+                inputName = tds.ElementAt(0).FindElement(By.TagName("input"));
+                inputAddress = tds.ElementAt(1).FindElement(By.TagName("input"));
+                //Assert.AreEqual("input", input1.TagName);
+
+                //this works
+                //Assert.AreEqual(true, inputName.GetAttribute("data-bind")=="value:Name");
+                
+                //looks like following won't work unless we access the underlying javascript object of the input DOM node - how to do that ?
+                Assert.AreEqual("Dileepa", inputName.GetAttribute("value"));
+
+                //Assert.AreEqual("Earth", inputAddress.GetAttribute("value"));
+            }
+            else
+                Assert.AreEqual("test", "fails");
+            //IWebElement input2 = tds.ElementAt(1).FindElement(By.TagName("input"));
+
+            
+
         }
         
     }
